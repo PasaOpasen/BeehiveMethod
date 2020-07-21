2a9bf610f5d6ec89264c496bea29db3a8bdae1c1
ДМ. Па <qtckpuhdsa@gmail.com>
2020-07-21 20:32:11 +0300

    first animations

--- Animate.py
+++ Animate.py
@@ -8,6 +8,11 @@ Created on Tue Jul 21 16:41:53 2020
 from Beehive import *
 import numpy as np
 import matplotlib.pyplot as plt
+import math
+import os
+
+
+route = os.path.join(os.path.dirname(__file__), 'images')
 
 
 def plot1d(func = TestFunctions.Rastrigin, begin = -5, end = 5, count = 15, title = 'Rastrigin function'):
@@ -15,36 +20,48 @@ def plot1d(func = TestFunctions.Rastrigin, begin = -5, end = 5, count = 15, titl
     def val(arr):
         return np.array([func(t) for t in arr])
     
+    
+    if not os.path.exists(route):
+        os.mkdir(route)
     x = np.linspace(begin,end,150)
     y = val(x)
     
-    plt.plot(x, y)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.suptitle(title)
+    def base_plot():
+        plt.plot(x, y)
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.suptitle(title)
+    
+    def base_save(number):
+        
+        r = os.path.join(route,f'{title}_{number}.png')
+        plt.savefig(r, dpi =350)
+        plt.show()
+        plt.close()
+    
+    arrow_width = 1
+    arrow_length = 4
+    xytext = (0, 40)  #(0, -60)
+    
+    
+    base_plot()
     plt.title('before start',color = 'red')
     #plt.text(-1, 35, r'before start', fontsize=14, color='red')
-    plt.show()
-    
+    base_save(0)
     
     #bees = Bees(x[np.newaxis,:])
     bees = Bees(np.random.uniform(begin,end,(count,1)), width = 3)
     
     now = bees.x.flatten()
     best = bees.bests.flatten()
-    #s1 = np.ones(count)
     
-    plt.plot(x, y)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.suptitle(title)
+    base_plot()
     plt.title('initialization',color = 'red')
     #plt.text(-1, 35, r'initialization', fontsize=14, color='red')
     
     plt.plot(best, val(best), 'bs', color = 'red')
     plt.plot(now, val(now), 'ro', color = 'green')
-    
-    plt.show()
+    base_save(1)
     
     
     hive = Hive(bees,func, verbose = True)
@@ -55,31 +72,27 @@ def plot1d(func = TestFunctions.Rastrigin, begin = -5, end = 5, count = 15, titl
     
     #pos = float(hive.best_pos)
     
-    plt.plot(x, y)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.suptitle(title)
+    base_plot()
     plt.title('first step',color = 'red')
     #plt.text(-1, 35, r'first step', fontsize=14, color='red')
     
-    plt.plot(best, val(best), 'bs', color = 'red')
-    plt.plot(now, val(now), 'ro', color = 'green')
+    plt.plot(best, val(best), 'bs', color = 'red', label = 'best bee positions')
+    plt.plot(now, val(now), 'ro', color = 'green', label = 'current bee positions')
     
-    plt.annotate('global min', xy=(float(hive.best_pos), func(hive.best_pos)), xytext=(3.5, 1),
+    plt.annotate('global min', xy=(float(hive.best_pos), func(hive.best_pos)), xytext=xytext,
              arrowprops=dict(facecolor='black', shrink=0.05),
              )
     
     for p, v in zip(now,v):
         tmp = func(p)
-        plt.arrow(p, tmp, v, 0, length_includes_head=True,
-          head_width=0.8, head_length=0.2
-          )
+        plt.arrow(p, tmp, v, 0 , width = arrow_width,length_includes_head = True,
+               head_length = min(arrow_length, math.fabs(v/3))
+              )
     
     
+    base_save(2)
     
-    plt.show()
-    
-    for i in range(2,8):
+    for i in range(2,15):
         
         hive.best_val, hive.best_pos = hive.bees.make_step(0.3, 2, 5, hive.best_pos, hive.best_val)
         
@@ -89,37 +102,49 @@ def plot1d(func = TestFunctions.Rastrigin, begin = -5, end = 5, count = 15, titl
         
         #pos = float(hive.best_pos)
         
-        plt.plot(x, y)
-        plt.xlabel('x')
-        plt.ylabel('y')
-        plt.suptitle(title)
+        base_plot()
         plt.title(f'step {i}',color = 'red')
         #plt.text(-1, 35, f'step {i}', fontsize=14, color='red')
         
 
         
-        plt.annotate('global min', xy=(float(hive.best_pos), func(hive.best_pos)), xytext=(3.5, 1),
+        plt.annotate('global min', xy=(float(hive.best_pos), func(hive.best_pos)), xytext=xytext,
                  arrowprops=dict(facecolor='black', shrink=0.05),
                  )
         
         for b, p in zip(best, now):
             plt.plot([b,p], val(np.array([b,p])), 'r--', color = 'blue')
             
-        plt.plot(best, val(best), 'bs', color = 'red')
-        plt.plot(now, val(now), 'ro', color = 'green')
+        plt.plot(best, val(best), 'bs', color = 'red', label = 'best bee positions')
+        plt.plot(now, val(now), 'ro', color = 'green', label = 'current bee positions')
         
         for p, v in zip(now,v):
             tmp = func(p)
-            plt.arrow(p, tmp, v, 0, length_includes_head=True,
-              head_width=0.8, head_length=0.2
+            plt.arrow(p, tmp, v, 0 , width = arrow_width,length_includes_head = True,
+               head_length = min(arrow_length, math.fabs(v/3))
               )
-        plt.show()
+        
+        plt.legend()
+        base_save(i+1)
     
     
 
-plot1d(TestFunctions.Shvel, -65,90, count = 8, title = "Shvel")
+tit = "Rastrigin function"
+
+#plot1d(TestFunctions.Shvel, -90,100, count = 8, title = tit)
+
+#plot1d(lambda arr: TestFunctions.Shvel(arr)+random.uniform(0,4), -90,100, count = 8, title = tit)
+
+plot1d(TestFunctions.Rastrigin, -6,7, count = 8, title = tit)
 
 
+import imageio
+images = []
+filenames = [os.path.join(route, f'{tit}_{i}.png') for i in range(15)]
+for filename in filenames:
+    images.append(imageio.imread(filename))
+    
+imageio.mimsave(os.path.join(route,f'{tit}_movie.gif'), images, duration = 0.4)
 
 
 
